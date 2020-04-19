4bf4370d242d95bb526b9afddfa19af216c4556b
CarlosRE <encina.carlos.ruben@gmail.com>
2020-04-19 19:25:49 -0300

    Bootstrap

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const exphbs = require('express-handlebars');
 
 // Initializations
 const app = express();
@@ -7,7 +8,13 @@ const app = express();
 // Settings
 app.set('port', process.env.PORT || 3000 );
 app.set('views', path.join(__dirname, 'views'));
-
+app.engine('.hbs', exphbs({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'),'layouts'),
+    partialsDir: path.join(app.get('views'),'partials'),
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
 
 
 // Middlewares
@@ -18,7 +25,7 @@ app.use(express.urlencoded({extended:false}));
 
 // Routes
 app.get('/', (req,res) => {
-    res.send('Hello Wolrd');
+    res.render('index');
 })
 
 // Static file
